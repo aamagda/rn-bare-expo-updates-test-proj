e7914bd1904e8dd447a22e31efae0f25e3aa1b59
MagdaAA <magdaaa@eruditor-group.com>
2025-02-11 13:48:56 +0300

    init expo-modules

--- AwesomeProject/ios/AwesomeProject/AppDelegate.mm
+++ AwesomeProject/ios/AwesomeProject/AppDelegate.mm
@@ -23,6 +23,30 @@
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+// return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
+
+// Unable to resolve module ./.expo/.virtual-metro-entry from /Users/magdaaa/projects/rn-bare-expo-updates-test-proj/AwesomeProject/.: 
+// None of these files exist:
+//   * .expo/.virtual-metro-entry(.ios.js|.native.js|.js|.ios.jsx|.native.jsx|.jsx|.ios.json|.native.json|.json|.ios.ts|.native.ts|.ts|.ios.tsx|.native.tsx|.tsx)
+//   * .expo/.virtual-metro-entry
+
+// RCTFatal
+// __28-[RCTCxxBridge handleError:]_block_invoke
+// _dispatch_call_block_and_release
+// _dispatch_client_callout
+// _dispatch_main_queue_drain
+// _dispatch_main_queue_callback_4CF
+// __CFRUNLOOP_IS_SERVICING_THE_MAIN_DISPATCH_QUEUE__
+// __CFRunLoopRun
+// CFRunLoopRunSpecific
+// GSEventRunModal
+// -[UIApplication _run]
+// UIApplicationMain
+// main
+// start_sim
+// 0x0
+// 0x0
+
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
